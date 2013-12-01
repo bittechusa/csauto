ae5617bec89923bf8297902c2868355e1d1ab7cd
Shahidul <sislam_31@yahoo.com>
2013-12-01 00:08:08 -0500

    added files

--- src/test/java/com/cs/ui/test/ShahidulTest.java
+++ src/test/java/com/cs/ui/test/ShahidulTest.java
@@ -5,11 +5,12 @@ import org.testng.annotations.Test;
 
 public class ShahidulTest extends BaseTest
 {
-	
+	Common support = new Common();
 	@Test
 	public void demo1()
 	{
-	
+		support.clickBuyTicket();
+		support.accessCodeHandle();
 		
 	}
 
