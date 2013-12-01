54a1d2ddd831120547de864d5d6adcd127e84f50
sn <shanaj.nizam@gmail.com>
2013-12-01 00:44:38 -0500

    shanaj-some modified2

--- src/test/java/com/cs/ui/test/ShTest.java
+++ src/test/java/com/cs/ui/test/ShTest.java
@@ -3,6 +3,8 @@ package com.cs.ui.test;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
+
+
 public class ShTest extends BaseTest
 {
 	Common support = new Common();
@@ -10,16 +12,55 @@ public class ShTest extends BaseTest
 	@Test
 	public void buyTicketExistingCustomers()
 	{
+		
+		
 		support.clickBuyTicket();
 		support.accessCodeHandle();
 		
 		driver.findElementByClassName("login_title").isDisplayed();
 		
 		
-		driver.findElementByClassName("required email").sendKeys("");
+		//Positive Test	
+		driver.findElement(By.name("login_email")).sendKeys("");
+		driver.findElement(By.name("login_password")).sendKeys("");
+		driver.findElement(By.xpath("//input[contains(@class,'action_button') and contains(@value,'Sign')]")).click();
+		
+		//Negative Test 1
+		/*int a =1;
+		
+		if (a==1)
+		{
+			
+		}*/
+		
+		driver.findElement(By.name("login_email")).sendKeys(""); //Type in incorrect email
+		driver.findElement(By.name("login_password")).sendKeys(""); //Type in correct password
+		System.out.println("");
+		driver.findElement(By.xpath("//input[contains(@class,'action_button') and contains(@value,'Sign')]")).click();
+				
+		//Negative Test 2
+		driver.findElement(By.name("login_email")).sendKeys(""); //Type in correct email
+		driver.findElement(By.name("login_password")).sendKeys(""); //Type in incorrect password
+		driver.findElement(By.xpath("//input[contains(@class,'action_button') and contains(@value,'Sign')]")).click();
+		
+		//Negative Test 3
+		driver.findElement(By.name("login_email")).sendKeys(""); //Type in incorrect email
+		driver.findElement(By.name("login_password")).sendKeys(""); //Type in incorrect password
+		driver.findElement(By.xpath("//input[contains(@class,'action_button') and contains(@value,'Sign')]")).click();
+		
 	}
 		
+	@Test
+	public void forgetPassword()
+	{
+		driver.findElement(By.xpath("//input[contains(@class,'login_reset')]")).click();		
+	}
 
+	@Test
+	public void passwordReminder()
+	{
+	driver.findElement(By.xpath("//input[contains(@class,'required email') and contains(@name,'email')]")).sendKeys("");//Type email address
 	
+	}
 
 }
