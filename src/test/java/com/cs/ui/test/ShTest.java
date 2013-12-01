0faf1d02698c35cda25e2557e138a51c996d4954
sn <shanaj.nizam@gmail.com>
2013-11-30 23:34:31 -0500

    shanaj-some modified

--- src/test/java/com/cs/ui/test/ShTest.java
+++ src/test/java/com/cs/ui/test/ShTest.java
@@ -6,10 +6,20 @@ import org.testng.annotations.Test;
 public class ShTest extends BaseTest
 {
 	@Test
-	public void demo1()
+	public void accessCode()
 	{
 		
+		try 
+		{
+			driver.findElement(By.className("codeinput")).sendKeys("test");
+		}
 		
+		catch (Exception NoException)
+		{
+			System.out.println("Test Code is incorrect");
+		}
+		
+		driver.findElement(By.className("codesubmit")).click();
 	}
 
 }
