9beeb4825e28ca6b96f70b9df58723c74ad6cfd7
Shahidul <sislam_31@yahoo.com>
2013-12-01 19:51:47 -0500

    addded file

--- src/test/java/com/cs/ui/test/ShahidulTest.java
+++ src/test/java/com/cs/ui/test/ShahidulTest.java
@@ -13,7 +13,8 @@ public class ShahidulTest extends BaseTest
 		support.beforeCart();
 		clickRemoveAllLink();
 		support.verifyElementPresent(By.xpath("//div[contains(@class,'success_wrap checkout_top_successmsg')]"), "Element Empty message");
-		
+		billingInfoExistingCustomerWithSameAddress();
+		clickRuturnBackButton();
 	}
 	
 	public void clickRemoveAllLink() throws InterruptedException
@@ -28,8 +29,49 @@ public class ShahidulTest extends BaseTest
 		{
 			System.out.println("Remove All link not found");
 		}	
+		
+		
 	}
-	
+
+	public void clickRuturnBackButton()
+	{
+		driver.findElement(By.xpath("//a[@class='action_button']")).click();
+	}
+	public void existingCustomer()
+	{
+		int ran;
+		ran = 100 + (int)(Math.random() * ((10000 - 100) + 1));
+		String email = "Signuptest" + ran + "@gmail.com";
+		String pass = "Signing" + ran ;
+		
+		driver.findElement(By.name("login_email")).sendKeys(email);
+		driver.findElement(By.name("login_password")).sendKeys(pass);
+		driver.findElement(By.xpath("//input[@value='Sign In']")).click();
+		
+		
+	}
+	public void billingInfoExistingCustomerWithSameAddress()
+	{
+		driver.findElement(By.name("bill_firstname")).sendKeys("asad");
+		driver.findElement(By.name("bill_lastname")).sendKeys("zaman");
+		driver.findElement(By.name("bill_addressline1")).sendKeys("45 Main St");
+		driver.findElement(By.name("bill_towncity")).sendKeys("Brooklyn");
+		driver.findElement(By.name("bill_countystate")).sendKeys("ny");
+		driver.findElement(By.name("bill_postzip")).sendKeys("11201");
+		driver.findElement(By.name("bill_country")).click();
+		driver.findElement(By.name("//span[@class='selectboxit-text']")).click();
+		driver.findElement(By.name("ship_sameasbill")).click();
+	}
+	public void shippingAddressDifferentAsBillingAddress()
+	{
+		driver.findElement(By.name("ship_addressline1")).sendKeys("123 main street");
+		driver.findElement(By.name("ship_towncity")).sendKeys("queens");
+		driver.findElement(By.name("ship_countystate")).sendKeys("ny");
+		driver.findElement(By.name("ship_postzip")).sendKeys("10567");
+		driver.findElement(By.xpath("//span[@id='langSelectBoxItArrowContainer']")).click();
+		driver.findElement(By.xpath("//ul[contains(@class,'selectboxit-list')]//li[id='222']")).click();
+		
+	}
 	
 	
 	
